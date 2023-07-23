e67e7b48649c2f4f133a58a2dc6b047fa04a4823
granicadmitrij <dimagranitsa@mail.ru>
2023-07-23 14:54:53 +0400

    assertions

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,4 +4,12 @@ public class Calculator {
     public Object add(int i, int i1) {
         return i + i1;
     }
+
+    public int multiply(int i, int i1) {
+        return i * i1;
+    }
+
+    public int divide(int i, int i1) {
+        return i / i1;
+    }
 }
